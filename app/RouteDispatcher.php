a39f9b290e3c3321571466c57a61288d97e77886
yuceltoluyag <ytoluyag@gmail.com>
2019-04-15 02:16:43 +0300

    Autoload Ayarlandı

---
+++ app/RouteDispatcher.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App;
+
+use AltoRouter;
+
+class RouteDispatcher
+{
+    protected $match;
+    protected $controller;
+    protected $method;
+
+    public function __construct(AltoRouter $router)
+    {
+        $this->match = $router->match();
+
+
+        if ($this->match) {
+
+            list($controller,$method) = explode('@',$this->match['target']);
+            $this->controller = $controller;
+            $this->method     = $method;
+            if(is_callable(array(new $this->controller,$this->method)))
+            {
+                call_user_func_array(array(new $this->controller,$this->method),
+                array($this->match['params']));
+
+            } else {
+
+                echo 'Böyle Bir Method {$this->method} u {$this->controller} içerisinde yazmamışşınız ki dostum ';
+            }
+
+        } else {
+
+            header($_SERVER['SERVER_PROTOCOL'] . 'Sayfa Bulunamadı');
+            view('errors/404');
+
+        }
+
+    }
+
+}
