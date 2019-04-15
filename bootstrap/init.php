b56883ab6361b1cc9ba1e71cbd1d3407ca0bf803
yuceltoluyag <ytoluyag@gmail.com>
2019-04-15 03:08:38 +0300

    İluminate Database Eklendi & Sınıfı Yazıldı

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -2,5 +2,6 @@
 
 if (!isset($_SESSION)) session_start();
 require_once __DIR__ . '/../app/config/_env.php';
+new \App\Classes\Database();
 require_once __DIR__ . '/../app/routing/routes.php';
 new \App\RouteDispatcher($router);
--- public/index.php
+++ public/index.php
@@ -5,3 +5,9 @@ require_once __DIR__ . '/../bootstrap/init.php';
 $app_name = getenv('APP_NAME');
 
 //echo $app_name;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$bul = Capsule::table('musteriler')->get();
+
+var_dump($bul);
