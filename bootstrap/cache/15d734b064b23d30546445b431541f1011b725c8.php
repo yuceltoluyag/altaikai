95067ff86498d3f50bf1b9d6b1043a362ae9ba8c
yuceltoluyag <ytoluyag@gmail.com>
2019-04-29 14:53:48 +0300

    Örnek Sayfa

--- bootstrap/cache/15d734b064b23d30546445b431541f1011b725c8.php
+++ bootstrap/cache/15d734b064b23d30546445b431541f1011b725c8.php
@@ -5,20 +5,19 @@
                                         src="https://s3.amazonaws.com/uifaces/faces/twitter/jsa/48.jpg"
                                         alt="User Image">
         <div>
-            <p class="app-sidebar__user-name">John Doe</p>
-            <p class="app-sidebar__user-designation">Frontend Developer</p>
+            <p class="app-sidebar__user-name">Yücel Toluyağ</p>
+            <p class="app-sidebar__user-designation">Coding My Life</p>
         </div>
     </div>
     <ul class="app-menu">
-        <li><a class="app-menu__item" href="<?php echo e(route('Anasayfa')); ?>"><i
-                        class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a>
-        </li>
-        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i
-                        class="app-menu__icon fa fa-laptop"></i><span class="app-menu__label">UI Elements</span><i
+        <li><a class="app-menu__item" href="/"><i class="app-menu__icon fa fa-dashboard"></i><span
+                        class="app-menu__label">Anasayfa</span></a></li>
+        <li class="treeview"><a class="app-menu__item" href="/admin/customers/customers" data-toggle="treeview"><i
+                        class="app-menu__icon fa fa-users"></i><span class="app-menu__label">Müşteriler</span><i
                         class="treeview-indicator fa fa-angle-right"></i></a>
             <ul class="treeview-menu">
-                <li><a class="treeview-item" href="bootstrap-components.html"><i class="icon fa fa-circle-o"></i>
-                        Bootstrap Elements</a></li>
+                <li><a class="treeview-item" href="/admin/customers/customers"><i class="icon fa fa-circle-o"></i>
+                        Müşteri Listesi</a></li>
                 <li><a class="treeview-item" href="https://fontawesome.com/v4.7.0/icons/" target="_blank"
                        rel="noopener"><i class="icon fa fa-circle-o"></i> Font Icons</a></li>
                 <li><a class="treeview-item" href="ui-cards.html"><i class="icon fa fa-circle-o"></i> Cards</a></li>
